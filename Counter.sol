f11d013018b25f123b7534c400713ee9d7f4cd8c
Kamil Kabiri <149384790+kamilsiu@users.noreply.github.com>
2025-03-04 22:09:50 +0530

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -5,6 +5,7 @@ contract Counter{
         uint256 count;
         string name;
         }
+
     mapping(address=>UserCount) private userCounters;
     event updateCount (address indexed user,uint256 newCount);
 
@@ -12,18 +13,23 @@ contract Counter{
         userCounters[msg.sender].count+=1;
         emit updateCount(msg.sender, userCounters[msg.sender].count);
     }
+
     function decrement()public{
         require(userCounters[msg.sender].count>0,"Count is 0 ");
         userCounters[msg.sender].count-=1;
         emit updateCount(msg.sender, userCounters[msg.sender].count);
     }
+
     function getCount()public view returns(uint256){
         return userCounters[msg.sender].count;
     }
+
     function setName(string memory _name) public {
         userCounters[msg.sender].name = _name;
     }
+
     function getUserDetails()public view returns (UserCount memory){
         return userCounters[msg.sender];
     }
+
 }
