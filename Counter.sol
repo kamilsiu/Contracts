7061973445f41aa6e73a61007b1ddb46e1874bf7
= <KAMIL>
2025-03-05 10:52:39 +0530

    Enhanced Contract

--- Counter.sol
+++ Counter.sol
@@ -4,18 +4,26 @@ contract Counter{
     struct UserCount{
         uint256 count;
         string name;
+        uint256[] incrementHistory;
         }
     mapping(address=>UserCount) private userCounters;
-    event updateCount (address indexed user,uint256 newCount);
+    event UpdateCount (address indexed user,uint256 newCount);
 
     function increment()public{
         userCounters[msg.sender].count+=1;
-        emit updateCount(msg.sender, userCounters[msg.sender].count);
+        userCounters[msg.sender].incrementHistory.push(userCounters[msg.sender].count);
+        emit UpdateCount(msg.sender, userCounters[msg.sender].count);
     }
     function decrement()public{
         require(userCounters[msg.sender].count>0,"Count is 0 ");
         userCounters[msg.sender].count-=1;
-        emit updateCount(msg.sender, userCounters[msg.sender].count);
+        emit UpdateCount(msg.sender, userCounters[msg.sender].count);
+    }
+    function resetCount()public {
+        userCounters[msg.sender].count = 0;
+        delete userCounters[msg.sender].incrementHistory;
+        emit UpdateCount(msg.sender, userCounters[msg.sender].count);
+
     }
     function getCount()public view returns(uint256){
         return userCounters[msg.sender].count;
@@ -25,5 +33,8 @@ contract Counter{
     }
     function getUserDetails()public view returns (UserCount memory){
         return userCounters[msg.sender];
+    }
+    function getHistory()public view returns(uint256[] memory){
+        return userCounters[msg.sender].incrementHistory;
     }
 }
