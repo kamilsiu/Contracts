c8bf0a83fcd470a91cd986ebbd2e651c5757e052
= <KAMIL>
2025-03-09 00:59:12 +0530

    Basic Token

---
+++ Token.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Tokens is ERC20{
+constructor()("KamilTokken","KmT"){
+    _mint(msg.sender,10*10^decimals());
+}
+}
