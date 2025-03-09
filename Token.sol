760a9272e16b9f408696d400729842aff02bbf78
= <kamilnissarzarga29@gmail.com>
2025-03-09 22:48:40 +0530

    Token creation

--- Token.sol
+++ Token.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Tokens is ERC20{
 constructor()("KamilTokken","KmT"){
+    // Creating token and sending it to contract deployer
     _mint(msg.sender,10*10**decimals());
 }
 }
